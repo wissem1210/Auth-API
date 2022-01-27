5ca2abc1562db28b3f2cbfbc4e04510407240af3
wissem <maalelwissem@gmail.com>
2022-01-27 15:24:34 +0100

    added refresh token model

---
+++ src/models/refresh-token.js
@@ -0,0 +1,22 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+  class RefreshToken extends Model {
+    static associate(models) {
+      RefreshToken.belongsTo(models['User']);
+    }
+  }
+
+  RefreshToken.init(
+    {
+      token: {
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'RefreshToken',
+    }
+  );
+  return RefreshToken;
+};
